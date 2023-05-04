56b505323af3b22cbe4b00639e02ffe540468f5c
GyanPrakashRaj <thecornerstandingman@gmail.com>
2023-05-04 13:35:37 +0530

    logs

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -10,6 +10,33 @@ require('dotenv').config();
 
 router.post('/signup',(req,res)=>{
     res.send('this is signup page page from authroutes');
-    console.log(req.body);
+    //console.log(req.body);
+    const  {name, email, password, dob} = req.body;
+    if (!email || !password || !name || !dob){
+        return res.status(422).send({error: "please fill all details"});
+    }
+
+    User.findOne({ email: email})
+    .then(
+        async (saveduser) =>{
+            if (saveduser){
+            return res.status(422).send({error: "email Exist"});
+        }
+        const user = new user({
+            name,
+            email,
+            password,
+            dob
+        })
+        try{
+            await user.save();
+            res.send({ message: "user saved"});
+        }
+        catch (err){
+            console.log('db err', err);
+            return res.status(422).send({error: err.message});
+        }
+    }
+    )
 })
 module.exports = router;
